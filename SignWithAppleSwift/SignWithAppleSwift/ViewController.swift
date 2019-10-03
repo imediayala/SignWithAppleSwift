7787c9a3e4e5df44a7f8e24c12b7357bdcf8632b
Daniel Ayala Jabon <dayalaja@everis.com>
2019-10-03 10:40:00 +0200

    Initial commit

---
+++ SignWithAppleSwift/SignWithAppleSwift/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SignWithAppleSwift
+//
+//  Created by Daniel Ayala Jabon on 03/10/2019.
+//  Copyright © 2019 imediayala. All rights reserved.
+//
+
+import UIKit
+import AuthenticationServices
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
